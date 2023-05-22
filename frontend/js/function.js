04165f2b86933a5778b10ea2559fb0135ca1f1a2
khoa.pham <khoa.pham@southtelecom.vn>
2023-05-22 23:05:47 +0700

    (feat)[tips] git cherry-pick -n hashid, git commit --amend --no-edit

--- frontend/js/function.js
+++ frontend/js/function.js
@@ -1,13 +1,10 @@
 // 1. Function parameter (hoisting, closure, parameter on default defined value)
 // You can call function by parentheses (function(){})(paramter1, parameter2)
 {
-    // version 1.4 => pass more argument than parameter into fCall()
+    // version 1.5 => exit or handle return of fCall, ...
     // Result in console
-    // khoaminhi
-    // arguments:  [Arguments] { '0': 11, '1': 33 }
-    // param1:  11 param3:  3
-    // joiewph
-
+    // => must be return end check to exit in parent function called
+    
     const khoaminhi = 'khoaminhi';
     const options = {};
     const param1 = 1, param3 = 3;
@@ -25,6 +22,7 @@
             let param1 = 11;
             const param3 = 33;
             options.fCall(param1, param3);
+            console.log('....end call fCall')
         }
     }
 
@@ -33,6 +31,43 @@
     //=========================================================\\
     //*/
 }
+
+
+
+
+// {
+//     // version 1.4 => pass more argument than parameter into fCall()
+//     // Result in console
+//     // khoaminhi
+//     // arguments:  [Arguments] { '0': 11, '1': 33 }
+//     // param1:  11 param3:  3
+//     // joiewph
+
+//     const khoaminhi = 'khoaminhi';
+//     const options = {};
+//     const param1 = 1, param3 = 3;
+
+//     const fCall = function(param11) {
+//         console.log(khoaminhi);
+//         console.log('arguments: ', arguments)
+//         console.log('param1: ', param11, 'param3: ', param3);
+//     }
+
+//     options.fCall = fCall;
+
+//     function fRoot(options = {}) {
+//         if (typeof options.fCall === 'function') {
+//             let param1 = 11;
+//             const param3 = 33;
+//             options.fCall(param1, param3);
+//         }
+//     }
+
+//     fRoot(options);
+//     console.log('joiewph')
+//     //=========================================================\\
+//     //*/
+// }
 
 
 
